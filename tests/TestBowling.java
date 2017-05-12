7b90f90e5c5c57ffd998192a0a6c54153419b599
somename <someemail>
2017-05-12 12:04:40 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -6,8 +6,13 @@ public class TestBowling {
 
 	@Test
 	public void testToCheckScore() {
-		Frame f = new Frame(1,5);
-		assertEquals(6, f.score());
+		Frame f = new Frame(1,4);
+		assertEquals(5, f.score());
 	}
-
+	
+	@Test
+	public void testToCheckIfStrike(){
+		Frame f = new Frame(10,0);
+		assertTrue(f.isStrike());
+	}
 }
